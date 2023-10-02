bb11f5391edfa4f10ef5e8481f20a35cf8cea729
João Vitor Miguez da Cunha <PET0147543@aluno.firjansenaisesi.com.br>
2023-10-02 09:17:18 -0300

    Commit inicial

---
+++ src/main/java/com/api_senai/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.api_senai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
